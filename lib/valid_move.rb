c913b3fb03a1a0921e3f1d2968b987deab6d3c28
Ssenkowski <scsenkowski@gmail.com>
2017-10-16 18:34:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  board[index] == X || board[index] == O
+  board[index] != " " || board[index] == ""
   index >= 8 || index <= 0 
 end
 
